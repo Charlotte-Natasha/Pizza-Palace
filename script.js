2e4c9e5512a24e26a8fa67fdf1c1499ee47d9d10
Charlotte Natasha <natashaonyango97@gmail.com>
2022-03-21 12:02:21 +0100

    added table and function prototype

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 $(document).ready(function(){
+function Pizza (size, crust, toppings, price, quantity){
+  this.size=size;
+  this.crust=crust;
+  this.toppings=toppings;
+  this.price=price;
+  this.quantity=quantity;
+}
+Pizza.prototype.calculatePrice=function(){
+
+}
+let buttons=$('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
     $('.card').hover(function(){
         $('.overlay', this).show();
         $('.overlay-p', this).show();
@@ -20,3 +53,12 @@ $(".card").hover(
       $(".overlay-p", this).hide();
     }
   );
+  <div class="prices">
+      <label for="size-select">Pizza Sizes....</label>
+      <select name="size" id="selectSize">
+        <option value="sizes">Pizza Sizes...</option>
+        <option value="small">Small... <span>600Ksh</span></option>
+        <option value="medium">Medium... <span>1000Ksh</span></option>
+        <option value="large">Large... <span>1200Ksh</span></option>
+      </select>
+    </div>
